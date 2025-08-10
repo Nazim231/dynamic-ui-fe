816dd0c888ff652d3e80d7f3cf3e7f65ba5dc36f
Nazim231 <nsn172002.nazim@gmail.com>
2025-08-10 13:59:32 +0530

    chore: import fixing

--- src/AppBuilder/index.tsx
+++ src/AppBuilder/index.tsx
@@ -1,13 +1,20 @@
-import { SidebarProvider, SidebarTrigger } from '@/shadcnComponents/ui/sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@shadcn/ui/sidebar';
 import { AppSidebar } from './sidebar';
+import { Separator } from '@shadcn/ui/separator';
 
 export function AppBuilder() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-      </main>
+      <SidebarInset>
+         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          </header>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
--- src/AppBuilder/sidebar.tsx
+++ src/AppBuilder/sidebar.tsx
@@ -1,25 +1,37 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/shadcnComponents/ui/sidebar";
-import { Link } from "react-router-dom";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@shadcn/ui/sidebar';
+import { Link } from 'react-router-dom';
 
 export function AppSidebar() {
-  return <Sidebar>
-    <SidebarHeader className="gap-0">
-      <span className="font-medium">App Builder</span>
-      <span className="text-sm text-gray-400">v1.0.0</span>
-    </SidebarHeader>
-    <SidebarContent>
-      <SidebarGroup>
-        <SidebarGroupContent>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton asChild>
-                <Link to={'/page'}>Page</Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroupContent>
-      </SidebarGroup>
-    </SidebarContent>
-    <SidebarFooter></SidebarFooter>
-  </Sidebar>
+  return (
+    <Sidebar>
+      <SidebarHeader className="gap-0 px-4 ">
+        <span className="font-medium">App Builder</span>
+        <span className="text-sm text-gray-400">v1.0.0</span>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to={'/page'}>Page</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
 }
